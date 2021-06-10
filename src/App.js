07fa0c96d4df291af6dd031f2d55f2f2b9c5f888
Aidan Kirvan <apkirvan@hotmail.com>
2021-06-10 17:36:25 +1000

    can render and change hat name and modifier based on button clicks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState} from 'react';
+import React, {useEffect, useReducer} from 'react';
 import initialStorage   from './data/initialStorage'
 // reducer-state
 import playerReducer    from './reducers/playerReducer.js'
@@ -10,6 +10,9 @@ import UpgradeStore     from './components/UpgradeStore';
 // utilities
 import loadPlayer       from './utils/loadPlayer' 
 import savePlayer       from  './utils/savePlayer'
+import { hats } from './data/hats';
+import { rods } from './data/rods';
+import { baits } from './data/baits';
 
 const App = () => {
   // const [player, setPlayer] = useState(initialStorage)
@@ -17,8 +20,6 @@ const App = () => {
     playerReducer,
     initialStorage
   )
-
-  const [input, setInput] = useState('')
 
 
   // onLoad
@@ -39,6 +40,25 @@ const App = () => {
     })
   }
 
+  const handleHat = (event) => {
+    dispatch({
+      type: 'setHat',
+      data: event.target.value
+    })
+  }
+
+  const printHat = (input) => {
+    return (
+      hats.find(i => i.id === parseInt(input)).name
+    )
+  }
+
+  const printHatMod= (input) => {
+    return (
+      hats.find(i => i.id === parseInt(input)).modifier
+    )
+  }
+
   return (
     <div className="App">
       <PlayerContainer />
@@ -49,6 +69,10 @@ const App = () => {
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={handleChange} ></input>
         <button type="submit" value={store.name}>Submit</button>
+        <button onClick={handleHat} value={4}>Buy pirate hat</button>
+        <button onClick={handleHat} value={1}>Buy bucket hat</button>
+        <p>Hat: {printHat(store.inventory.hatId)}</p>
+        <p>Hat Mod: {printHatMod(store.inventory.hatId)}</p>
       </form>
     </div>
   );
