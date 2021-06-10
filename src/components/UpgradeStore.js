f0a2b35cd7a0eee19df4f5ad8545c6fd833e0f55
JaredGold <jegoldstein94@gmail.com>
2021-06-10 22:24:43 +1000

    add more syles

--- src/components/UpgradeStore.js
+++ src/components/UpgradeStore.js
@@ -1,10 +1,12 @@
+import {StoreDiv} from '../styles/style'
+
 //This is where the upgrade store is, updates the user's money and inventory when an upgrade is purchased
 
 const UpgradeStore = () => {
-    return(
-        <>
-        <h1>Shop</h1>
-        </>
-    )
+  return(
+    <StoreDiv>
+      <h1>Shop</h1>
+    </StoreDiv>
+  )
 }
 export default UpgradeStore
--- src/styles/style.js
+++ src/styles/style.js
@@ -1,17 +1,23 @@
 import styled from 'styled-components'
 
+// variables
+
+const mainColor = "#932944"
+const mainFontColor = "#fff"
+
 export const StyledFishBox = styled.div`
 ` 
 
 export const LeftBar = styled.div`
   height: 100vh;
-  background-color: green;
+  background-color: ${mainColor};
   grid-column-start: 1;
   display: grid;
   justify-content: center;
 `
 
 export const MainSection = styled.div`
+  color: ${mainFontColor};
   height: 100vh;
   background-color: red;
   grid-column-start: 2;
@@ -22,4 +28,13 @@ export const GameContainer = styled.div`
   max-height: 100vh;
   display: grid;
   grid-auto-flow: column;
+`
+
+export const StoreDiv = styled.div`
+  background-color: white;
+  margin-top: 10%;
+  width: 100%;
+  border: solid black 2px;
+  display: flex;
+  justify-content: center;
 `
