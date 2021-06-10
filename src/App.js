bc3e9adb24c374cfede09aa972a48829b45614cf
kaprestridge <kaprestridge@gmail.com>
2021-06-10 13:53:15 +1000

    boilerplate page elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import CurrentFishBox from './components/CurrentFishBox';
+import PlayerContainer from './components/PlayerContainer';
+import PlayerInventory from './components/PlayerInventory';
+import UpgradeStore from './components/UpgradeStore';
+
+//components
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <PlayerContainer />
+        <CurrentFishBox/>
+        <UpgradeStore />
+        <PlayerInventory />
+      </div>
     </div>
   );
 }
