5c2f3c3ba9c806a8df5b51b4c1efe7689ca05858
Aidan Kirvan <apkirvan@hotmail.com>
2021-06-11 10:12:50 +1000

    shop functions in new utils file

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
         <>
           <PlayerContainer />
           <CurrentFishBox/>
-          <UpgradeStore />
+          <UpgradeStore store={store} dispatch={dispatch} />
           <PlayerInventory />
         </>:
         <FirstLoad store={store} dispatch={dispatch} setHasName={setHasName}/>
